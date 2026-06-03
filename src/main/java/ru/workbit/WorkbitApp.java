77a7b58682b5981ea2e06ca3b14fd32b06d42bbd
Nikolay Aleksandrov <nkl.aleks@yandex.ru>
2026-06-03 15:56:58 +0300

    feat: default settings

---
+++ src/main/java/ru/workbit/WorkbitApp.java
@@ -0,0 +1,11 @@
+package ru.workbit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkbitApp {
+    static void main(String[] args) {
+        SpringApplication.run(WorkbitApp.class, args);
+    }
+}
